20a94416313e6734ac53aa4ba59e2408923d2de6
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-04-11 13:58:29 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -7,14 +7,14 @@ file = st.file_uploader("Please select a file to upload")
 #file = st.file_uploader("Upload file Excel", type=["xlsx", "xls"])
 url = 'https://raw.githubusercontent.com/Dtscience80/Project_ISAST_2022/main/df_minmax.csv'
 data = pd.read_csv(url)
-st.text("Berikut tabel data anda :") 
+
 if file is not None:
     #Can be used wherever a "file-like" object is accepted:
     #df= pd.read_csv(file)
-    df = pd.read_excel(file)
-    
+    df = pd.read_excel(file) 
     st.dataframe(data.head())
 
-st.text("Berikut deskripsi data anda :") 
+st.text("Berikut tabel data anda :") 
 st.dataframe(data, width=1000)
+st.text("Berikut deskripsi data anda :") 
 st.dataframe(data.describe())
