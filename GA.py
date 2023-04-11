68c2dc97dfbb7a21759468dd63d3265a94226b8e
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-04-11 13:42:49 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -2,16 +2,19 @@ import streamlit as st
 import pandas as pd
 
 st.title('Streamlit Genetic Algorithm')
-#upload single file and display it as a dataframe
-file = st.file_uploader("Please select a file to upload")
-#file = st.file_uploader("Upload file Excel", type=["xlsx", "xls"])
+st.sidebar.text('Feature Seleksi ')
+st.sidebar.text('Genetic Algorithm ')
+#file = st.file_uploader("Please select a file to upload")
+file = st.file_uploader("Upload file Excel", type=["csv", "xls"])
 url = 'https://raw.githubusercontent.com/Dtscience80/Project_ISAST_2022/main/df_minmax.csv'
 data = pd.read_csv(url)
 if file is not None:
     #Can be used wherever a "file-like" object is accepted:
     #df= pd.read_csv(file)
     df = pd.read_excel(file)
+    st.subheader(" Berikut tabel data anda: ")
     st.dataframe(data)
 
+    st.subheader(" Berikut deskripsi data anda: ")
 st.dataframe(data, width=1000)
 st.dataframe(data.describe())
