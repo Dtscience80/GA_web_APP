ecd1d0dd0759aa83ffb4a30d59c6f77b39aab58e
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-04-11 13:37:24 +0700

    Add files via upload

--- GA.py
+++ GA.py
@@ -3,21 +3,15 @@ import pandas as pd
 
 st.title('Streamlit Genetic Algorithm')
 #upload single file and display it as a dataframe
-#file = st.file_uploader("Please select a file to upload")
-file = st.file_uploader("Upload file CSV", type=["CSV", "csv"])
+file = st.file_uploader("Please select a file to upload")
+#file = st.file_uploader("Upload file Excel", type=["xlsx", "xls"])
 url = 'https://raw.githubusercontent.com/Dtscience80/Project_ISAST_2022/main/df_minmax.csv'
 data = pd.read_csv(url)
 if file is not None:
     #Can be used wherever a "file-like" object is accepted:
     #df= pd.read_csv(file)
     df = pd.read_excel(file)
-    st.dataframe(df)
+    st.dataframe(data)
 
 st.dataframe(data, width=1000)
 st.dataframe(data.describe())
-
-st.sidebar.text('Feature seleksi dengan ALgoritma ')
-st.sidebar.text('Genetic ALgorithm')
-header = data.columns.tolist()
-st.subheader(" feature data anda : ")
-st.text(header)
