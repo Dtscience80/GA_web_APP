70faa199ed87350747922a72dd9c6334d531cd48
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-05-06 10:06:00 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -207,8 +207,8 @@ rf = pd.DataFrame()
 rf["labels"] = labels
 rf["Score"] = fet_imp
 #display dataframe
-st.dataframe(df)
-#st.write("Feature selected : ", labels)
+#st.dataframe(df)
+st.write("Feature selected : ", rf)
 #st.write("Feature Score : ", fet_imp)
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 3))
