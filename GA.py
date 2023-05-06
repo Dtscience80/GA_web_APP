c74a3903e5a6442ee77ce4346c48651e250df11b
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-05-06 10:19:49 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -80,16 +80,22 @@ html_temp = """
 stc.html(html_temp)
 
 #upload single file and display it as a dataframe
-#st.write('Please select a file to upload:', #008000)
-file = st.file_uploader("Please select a file to upload")
+#file = st.file_uploader("Please select a file to upload")
 #file = st.file_uploader("Upload file Excel", type=["xlsx", "xls"])
-url = 'https://raw.githubusercontent.com/Dtscience80/Project_ISAST_2022/main/df_minmax.csv'
-data = pd.read_csv(url)
+#url = 'https://raw.githubusercontent.com/Dtscience80/Project_ISAST_2022/main/df_minmax.csv'
+#data = pd.read_csv(url)
+
+#Multiple files
+#adding a file uploader to accept multiple CSV file
+uploaded_files = st.file_uploader("Please select a CSV file", accept_multiple_files=True)
+for file in uploaded_files:
+    data = pd.read_csv(file)
+    st.write("File uploaded:", file.name)
 
 if file is not None:
     #Can be used wherever a "file-like" object is accepted:
     #df= pd.read_csv(file)
-    df = pd.read_excel(file) 
+    data = pd.read_excel(file) 
     st.dataframe(data.head())
 
 st.text("Berikut tabel data anda :") 
