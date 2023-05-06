a2cdc1dd2a702261a145909647c37f079d85ff15
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-05-06 13:26:03 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -117,7 +117,7 @@ for file in uploaded_files:
          # Mengganti delimiter menjadi koma
          data = temp.applymap(lambda x: str(x).replace(delimiter, ','))
        else:    
-         data = pd.read_csv(file, sep=',')
+         data = pd.read_csv(file)
        st.write("File uploaded:", file.name)
        st.dataframe(data.head())
        # Hapus baris dengan data kosong atau tidak dikenali
