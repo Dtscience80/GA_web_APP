877c89870f2e7a23e7cedb9bf435e959843605ec
Dtscience80 <101234762+Dtscience80@users.noreply.github.com>
2023-05-06 13:31:06 +0700

    Update GA.py

--- GA.py
+++ GA.py
@@ -103,8 +103,6 @@ data =[]
 uploaded_files = st.file_uploader("Please select a CSV/xlsx/xlx file", accept_multiple_files=True)
 for file in uploaded_files:
     if uploaded_files is not None:
-       #data = pd.read_csv(file)
-       # Membaca file CSV dengan delimiter ';', ':', atau spasi
        #data = pd.read_csv(file, sep='[;:\s]+', engine='python')
        for d in [',', ';', '\t', '|']:
           try:
@@ -117,10 +115,9 @@ for file in uploaded_files:
          # Mengganti delimiter menjadi koma
          data = temp.applymap(lambda x: str(x).replace(delimiter, ','))
        else:    
-         data = pd.read_csv(file)
+         data = pd.read_csv(file, sep=',', engine='python')
        st.write("File uploaded:", file.name)
        st.dataframe(data.head())
-       # Hapus baris dengan data kosong atau tidak dikenali
        data = data.dropna()
 
        st.text("Berikut tabel data anda :") 
